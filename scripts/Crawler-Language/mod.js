e1343a4bf129a56e585e39b67116336523a1ae23
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2022-05-04 22:14:56 +0300

    ...

--- scripts/Crawler-Language/mod.js
+++
@@ -1,5 +0,0 @@
-var meta = Vars.mods.locateMod("crawler-language").meta;
-meta.author = "[#0096FF]xzxADIxzx";
-meta.displayName = "Crawler Langu";
-meta.description = "adone [purple]langu[] toi [accent]Mindustry[]" +
-				   "[orange]\n\n   - GitHub\n   - Mobile Support";
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,8 @@
-require("Crawler-Language/mod");
-
 Events.on(EventType.ClientLoadEvent, e => {
 	var dialog = Vars.ui.language;
-	var button = new TextButton("Crawler", Styles.clearTogglet);
+	var button = new TextButton("crawler langu", Styles.clearTogglet);
 	button.clicked(() => {
-	    //if(dialog.getLocale().equals(loc)) return;
+		if (Core.settings.getString("locale") == "cw") return;
 	    Core.settings.put("locale", "cw");
 	    Log.info("Setting locale: @", "cw");
 	    Vars.ui.showInfo("@language.restart");
@@ -17,28 +15,23 @@ Events.on(EventType.ClientLoadEvent, e => {
 	}).size(400, 50).row();
 });
 
-// Change Core.bundle
-if (Core.settings.getString("locale") != "cw")
-	return;
+// why download a mod if you play in another language?
+if (Core.settings.getString("locale") != "cw") return;
 
+// translate meta
+var meta = Vars.mods.locateMod("crawler-language").meta;
+meta.author = "[#0096FF]xzxADIxzx";
+meta.displayName = "Crawler Langu ";
+meta.description = "adone [purple]langu[] toi [accent]Mindustry[]"
+
+// change Core.bundle
 var file = Vars.mods.locateMod("crawler-language").root.child("bundles").list()[0];
 PropertiesUtils.load(Core.bundle.getProperties(), file.reader());
-update_content();
-
-function update_content() {
-	var content = Vars.content;
-	content.items().items.forEach(item => update(item) );
-	content.liquids().items.forEach(item => update(item) );
-	content.blocks().items.forEach(item => update(item) );
-	content.units().items.forEach(item => update(item) );
-	content.statusEffects().items.forEach(item => update(item) );
-	content.sectors().items.forEach(item => update(item) );
-	content.planets().items.forEach(item => update(item) );
-}
+Vars.content.each(item => update(item)); // update localized strings
 
 function update(item){
 	if (item == null) return; // idk why but on mobile something went wrong
-	if (item.isHidden()) return; // Don`t update if item hidden... like "none"
+	if (item.isHidden()) return; // don`t update if item hidden... like "none"
 	var type = item.getContentType();
 	item.localizedName = Core.bundle.get(type + "." + item.name + ".name");
 	item.description = Core.bundle.getOrNull(type + "." + item.name + ".description");
